2ebc361b6dd007e1c483ef694554a32c4263fe4b
Christian Roberts <christian.m.roberts@outlook.com>
2020-05-07 10:34:51 -0400

    Incorporated a view component for UserDataDisplay in the shared layout

---
+++ Adventure Game/Components/UserDataDisplayViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Adventure_Game.Models;
+
+namespace Adventure_Game.Components
+{
+    public class UserDataDisplayViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+        public UserDataDisplayViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public string Invoke()
+        {
+            return "UserDataDisplay will manifest here:" +
+                "\nUserId:" +
+                "\nName:" +
+                "\nScreen:" +
+                "\nLevel:" +
+                "\nExp:" +
+                "\nHP:";
+        }
+        public IViewComponentResult InvokeusethisinplaceofInvokelater()//will replace Invoke()
+        {
+
+            return View(_context);
+
+        }
+    }
+}
